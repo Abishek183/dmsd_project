1ddf64359b28c6bbdb9fd8ed7878355a8301a4a0
Mihir Rana <78051985+MPR-GH@users.noreply.github.com>
2024-05-01 15:08:24 -0400

    Update addReader.php

--- Admin/addReader.php
+++ Admin/addReader.php
@@ -3,6 +3,19 @@ error_reporting(E_ALL & ~E_NOTICE);
 // Include database connection file
 include_once "../db.php";
 
+function generateRandomString($length = 10) {
+    $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    $charLength = strlen($characters);
+    $randomString = '';
+
+    // Generate random characters
+    for ($i = 0; $i < $length; $i++) {
+        $randomString .= $characters[rand(0, $charLength - 1)];
+    }
+
+    return $randomString;
+}
+
 // Check if form is submitted
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     // Retrieve input data
@@ -13,15 +26,28 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
     // Query to find available copies for the document at the branch
     
+    // Insert into READER table
     $query = "INSERT INTO READER (RTYPE, RNAME, RADDRESS, PHONE_NO) VALUES ('$rtype', '$rname', '$raddress', '$rphone')";
-    
-    // Execute the query
     $result = mysqli_query($conn, $query);
 
-    if ($result == TRUE)    {
-        echo "Successfully Added Reader";
-    }   else    {
-        echo "Error: Reader already exists";
+    $cardNumber = generateRandomString(10);
+
+    if ($result) {
+        // Retrieve the auto-generated UserID
+        $userID = mysqli_insert_id($conn);
+        echo "Successfully Added Reader. UserID: " . $userID;
+        
+        // Insert into USERSLIB table using the retrieved UserID
+        $query = "INSERT INTO USERSLIB (USERREADERID, USERTYPE, CARDNUMBER, PASSWORD) VALUES ('$userID', 'reader', '$cardNumber', NULL)";
+        $result = mysqli_query($conn, $query);
+        
+        if ($result) {
+            //echo "Successfully Added Reader to USERSLIB";
+        } else {
+            //echo "Error: " . mysqli_error($conn);
+        }
+    } else {
+        //echo "Error: " . mysqli_error($conn);
     }
 
     // Close connection
